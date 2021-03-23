72263be315a3c80d5c48ab8e35c739a14b9034a2
Brinza Bezrukoff <bezrukoff888@gmail.com>
2021-03-24 00:28:26 +0300

    Refactored

--- tg.py
+++ tg.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 import telebot
 from telebot import types
-import urllib
+import requests
 from io import BytesIO
-from PIL import Image
-from collections import deque
+
+from logic import shunting_yard, calculate
+
+
+r = requests.get('https://www.meme-arsenal.com/memes/ddcf6ef709b8db99da11efd281abd990.jpg')
+MEM_IMAGE = BytesIO(r.content)  # BytesIO creates file-object from bytes string
+
+operators = {
+    "~": ("Логическое отрицание",),
+    "&": ("Конъюнкция",),
+    "|": ("Дизъюнкция",),
+    ">": ("Импликация",),
+    "^": ("Исключающее или",),
+    "=": ("Эквиваленция",),
+}
+
+HELP_STR = "\n".join([f"<b>{op}</b> {op_info[0]}" for op, op_info in operators.items()])
+
 
 with open("token.txt") as tk:
     bot = telebot.TeleBot(tk.read())
@@ -17,30 +33,39 @@ def start_message(message):
     send_mess = f"<b>Привет {message.from_user.first_name} {message.from_user.last_name}!</b>\nНажми на кнопку, чтобы узнать, что я умею"
     bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
 
-def Artem(StepaHuesos):
-    pass
+
+@bot.message_handler(regexp="help|ops|помощь")
+def send_help(message):
+    bot.send_message(message.chat.id, HELP_STR, parse_mode='html')
+
+
+@bot.message_handler(regexp="&|\||>|~|\^|=")
+def handle_ops(message):
+    out, variables = shunting_yard(message.text)
+    print(out)
+    bot.send_message(message.chat.id, str(out))
+
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     get_message_bot = message.text.strip().lower()
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
 
     if get_message_bot == 'помощь':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         markup.add(types.KeyboardButton('Список операторов'))
         final_message = 'Я бот, который умеет строить таблицы истинности'
-        bot.send_message(message.chat.id, final_message, parse_mode='html', reply_markup=markup)
     elif get_message_bot == 'список операторов':
-        final_message = '<b>Выберите команду:</b> \n <b>~</b> : Логическое отрицание \n <b>&</b> : Конъюнкция \n <b>|</b> : Дизъюнкция \n <b>></b> Импликация \n <b>^</b> : Исключающее или \n <b>=</b> : Эквиваленция'
-        bot.send_message(message.chat.id, final_message, parse_mode='html' )
+        final_message = HELP_STR
     elif 'гей' in get_message_bot:
-        final_message = f"Артем, сам гей"
-        bot.send_message(message.chat.id, final_message, parse_mode='html')
+        final_message = f"{message.from_user.first_name}, сам гей!"
     else:
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
         markup.add(types.KeyboardButton('Помощь'))
-        final_message = BytesIO(urllib.request.urlopen('https://www.meme-arsenal.com/memes/ddcf6ef709b8db99da11efd281abd990.jpg').read())
-        bot.send_photo(message.chat.id, final_message)
-def artemlox():
-    pass
+        final_message = MEM_IMAGE
+
+    if isinstance(final_message, BytesIO):
+        bot.send_photo(message.chat.id, final_message, reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, final_message, parse_mode='html', reply_markup=markup)
+
 
 bot.polling(none_stop=True)
