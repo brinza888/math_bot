471b601fd4d96607dfa332da34dee87c830ac41e
Ilya Bezrukov <bezrukoff888@gmail.com>
2022-03-23 21:52:08 +0300

    Added license and copyright notices

--- safe_eval.py
+++ safe_eval.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2021-2022 Ilya Bezrukov, Stepan Chizhov, Artem Grishin
+#
+# This file is part of math_bot.
+#
+# math_bot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# any later version.
+#
+# math_bot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import ast
 import operator as op
 from functools import wraps
@@ -54,6 +73,8 @@ def _eval(node):
 
 
 if __name__ == '__main__':
+    print("Copyright (C) 2021-2022 Ilya Bezrukov, Stepan Chizhov, Artem Grishin")
+    print("Licensed under GNU GPL-2.0-or-later")
     while True:
         try:
             print(safe_eval(input("> ")))
