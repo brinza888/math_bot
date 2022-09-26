d9b7e15524cbfa493a43c1b3cc45a4d9380d91fd
Ilya Bezrukov <bezrukoff888@gmail.com>
2022-09-26 17:12:30 +0300

    Fixed #38 (broadcast command fails)

--- tg.py
+++ tg.py
@@ -21,6 +21,7 @@ from io import StringIO
 
 import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from telebot.apihelper import ApiTelegramException
 
 from git import Repo
 
@@ -366,8 +367,14 @@ def broadcast_input(message):
 
 def broadcast(message):
     db = get_db()
+    blocked_count = 0
     for user in db.query(User).all():
-        bot.send_message(user.id, message.text)
+        try:
+            bot.send_message(user.id, message.text)
+        except ApiTelegramException:
+            blocked_count += 1
+    bot.send_message(message.chat.id, "Рассылка успешно завершена!\n"
+                                      f"Не получили рассылку: {blocked_count}")
     close_db()
 
 
