63d14a7d2b0e98562612faf3a0815f4df3290ea3
Ilya Bezrukov <bezrukoff888@gmail.com>
2022-10-03 01:43:09 +0300

    Add warning about power operator turned off

--- tg.py
+++ tg.py
@@ -334,7 +334,7 @@ def euclid_output(message):
 
 @bot.message_handler(commands=["calc"])
 def calc_input(message):
-    m = bot.send_message(message.chat.id, "Операция возведения в степень обозначается <b>^</b>\n\n"
+    m = bot.send_message(message.chat.id, "Операция возведения в степень временно недоступна!\n"
                                           "Введите выражение:",
                          parse_mode="html")
     bot.register_next_step_handler(m, calc_output)
