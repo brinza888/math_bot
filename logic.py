a8341734cc689de7622e30bd5825bc2ab83f3f29
Ilya Bezrukov <bezrukoff888@gmail.com>
2022-10-14 00:58:29 +0300

    Re-write logic.py with shunting_yard.py usage

--- logic.py
+++ logic.py
@@ -17,89 +17,54 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from collections import deque
+
+from shunting_yard import ShuntingYard, Operator, CalculationLimitError
+from config import Config
 
 
-OPS = {
-    "~": (0, 1, lambda x: not x, "Отрицание (НЕ)"),
-    "&": (1, 2, lambda x, y: x and y, "Конъюнкция (И)"),
-    "|": (2, 2, lambda x, y: x or y, "Дизъюнкция (ИЛИ)"),
-    ">": (3, 2, lambda x, y: not x or y, "Импликация (следовательно)"),
-    "^": (4, 2, lambda x, y: x != y, "Исключающее ИЛИ (XOR)"),
-    "=": (4, 2, lambda x, y: x == y, "Эквиваленция (тождество)")
-}
+def logic_converter(x: str):
+    v = int(x)
+    if v not in [0, 1]:
+        raise ValueError("Constant must be a logic value: 1 (true) or 0 (false)")
+    return bool(v)
 
 
-def shunting_yard(s):
-    variables = []
-    right = deque(s.replace(" ", ""))
-    stack = []
-    out = []
-    while right:
-        t = right.popleft()
-        if t in variables:
-            out.append(t)
-        elif t.isdigit():
-            out.append(bool(t))
-        elif t in OPS:
-            while len(stack) > 0 and stack[-1] in OPS and OPS[stack[-1]][0] <= OPS[t][0]:
-                out.append(stack.pop())
-            stack.append(t)
-        elif t == "(":
-            stack.append(t)
-        elif t == ")":
-            while stack[-1] != "(":
-                out.append(stack.pop())
-            stack.pop()
-        else:
-            if t not in variables:
-                variables.append(t)
-            out.append(t)
-    while stack:
-        out.append(stack.pop())
-    return out, variables
+logicSY = ShuntingYard(
+    [
+        Operator("~", lambda a: not a, 20, ary=Operator.Ary.UNARY),  # NOT
+        Operator("&", lambda a, b: a and b, 10),  # AND
+
+        Operator("|", lambda a, b: a or b, 5),  # OR
+        Operator("^", lambda a, b: a != b, 5),  # XOR
+
+        Operator(">", lambda a, b: not a or b, 2),  # IMP
+        Operator("=", lambda a, b: a == b, 1),  # EQU
+    ],
+    [],
+    use_variables=True,
+    converter=logic_converter
+)
 
 
-def calculate(tokens, variables):
-    stack = []
-    for t in tokens:
-        if t in OPS:
-            operands = deque()
-            for i in range(OPS[t][1]):
-                if len(stack) == 0:
-                    raise SyntaxError("Incorrect logic expression")
-                operands.appendleft(stack.pop())
-            stack.append(OPS[t][2](*operands))
-        elif t.isalpha():
-            if t in variables:
-                stack.append(bool(variables[t]))
-            else:
-                raise NameError(f"Variable \"{t}\" not defined!")
-        else:
-            stack.append(t)
-
-    if not stack or len(stack) > 1:
-        raise SyntaxError("Incorrect logic expression")
-    return stack[-1]
-
-
-def build_table(s, var_limit=8):
-    tokens, variables = shunting_yard(s)  # kowalski analysis
+def build_table(expr):
+    pexpr = logicSY.parse(expr)
+    logicSY.shunt(pexpr)
+    variables = list(sorted(pexpr.variables))
     n = len(variables)
-    if n > var_limit:
-        raise ValueError("Variables limit reached")
+    if n > Config.MAX_VARS:
+        raise CalculationLimitError("Variables count limit reached")
     table = []
     for i in range(2 ** n):
         values = [int(x) for x in bin(i)[2:].rjust(n, "0")]
-        d = {variables[k]: values[k] for k in range(n)}
-        table.append(values + [int(calculate(tokens, d))])
+        vars_ = {k: v for k, v in zip(variables, values)}
+        table.append(values + [int(pexpr.eval(vars_))])
     return table, variables
 
 
 if __name__ == "__main__":
     print("Copyright (C) 2021-2022 Ilya Bezrukov, Stepan Chizhov, Artem Grishin")
     print("Licensed under GNU GPL-2.0-or-later")
-    s = input("Expression: ")
+    s = input("> ")
     table, variables = build_table(s)
     print(*variables, "F")
     for row in table:
