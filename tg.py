462411081d6285df9da3b0224f6d3cda713f04f3
Ilya Bezrukov <bezrukoff888@gmail.com>
2023-01-14 22:28:11 +0300

    Fix #47, .infinity_polling instead of just .polling

--- tg.py
+++ tg.py
@@ -538,4 +538,4 @@ def back_func(call):
 if __name__ == "__main__":
     print("Copyright (C) 2021-2022 Ilya Bezrukov, Stepan Chizhov, Artem Grishin")
     print("Licensed under GNU GPL-2.0-or-later")
-    bot.polling(none_stop=True)
+    bot.infinity_polling()  # should be infinity to avoid exceptions (#47)
